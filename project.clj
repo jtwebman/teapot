b47000ae1ea01e3d04255b3a5b1e94aabf10d453
JT Turner <jtwebman@gmail.com>
2015-12-03 11:38:26 -0800

    first commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject teapot "1.0.0-Teapot"
+  :description "My first app because I'm a little teapot."
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main teapot.core)
---
+++ test/teapot/test/core.clj
@@ -0,0 +1,6 @@
+(ns teapot.test.core
+  (:use [teapot.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
